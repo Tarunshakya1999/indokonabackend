dddf305437b5cf7b636e8a60fdf381613bd20ccf
TarunShakya <shakyatarun32@gmail.com>
2025-10-05 15:46:38 +0530

    Initial Django proiouhhhbju

--- backend/settings.py
+++ backend/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  
     "http://127.0.0.1:3000",
+    "https://indokonafrontend.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
