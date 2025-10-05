0fe24657a258aa296e1a1aa6e635826f3cf4f6ae
TarunShakya <shakyatarun32@gmail.com>
2025-10-05 15:42:08 +0530

    Initial Django proioubbbbju

--- backend/settings.py
+++ backend/settings.py
@@ -212,3 +212,10 @@ SIMPLE_JWT = {
     "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'shakyatarun32@gmail.com'
+EMAIL_HOST_PASSWORD = 'bolq hahz glyw uzlr'  # App Password (NOT Gmail password!)
