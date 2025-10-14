1f29f599d4ed8b10b9810ce564193a54ca726ce3
TarunShakya <shakyatarun32@gmail.com>
2025-10-15 00:51:58 +0530

    Tuiyt78867868i

--- backend/settings.py
+++ backend/settings.py
@@ -99,12 +99,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default="postgresql://indokonadatabase_user:XgLvGcjumRFi1m63jPabFFjnnu4YwnqN@dpg-d3mk042li9vc738412qg-a/indokonadatabase",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
