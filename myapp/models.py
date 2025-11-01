f1d06d0026ab971f44f74f398234f3091f6cc0c9
TarunShakya <shakyatarun32@gmail.com>
2025-11-01 15:06:14 +0530

    New N89iii2

--- myapp/models.py
+++ myapp/models.py
@@ -64,7 +64,7 @@ class PDF(models.Model):
 class DigitalProducts(models.Model):
     productname = models.TextField()
     productdescription = models.CharField(max_length=1000)
-    productprice = models.IntegerField()
+    productprice = models.IntegerField() 
     productdiscounted_price = models.IntegerField()
     productimg = models.ImageField(upload_to="uploadimage")
     productrating = models.PositiveSmallIntegerField(default=5) 
