7f238f4276869321f1893b4dda630ea0ea87af95
TarunShakya <shakyatarun32@gmail.com>
2025-11-08 12:49:52 +0530

    new cha87687tuu

--- myapp/models.py
+++ myapp/models.py
@@ -199,21 +199,3 @@ class HotDeal(models.Model):
 
 
 
-
-# Signals to create profile automatically
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-  if created:
-    Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-  instance.profile.save()
-
-
-
