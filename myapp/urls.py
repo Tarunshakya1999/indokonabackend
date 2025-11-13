0350305c037d00bd4b03b265d31eb51aa36a6df2
TarunShakya <shakyatarun32@gmail.com>
2025-11-13 13:13:22 +0530

    new ckkke

--- myapp/urls.py
+++ myapp/urls.py
@@ -11,13 +11,13 @@ router.register(r'product',ProductAPI)
 router.register(r'cart', CartAPI)
 router.register('blogs', BlogViewSet)
 # New Urls
-router.register('products', ProductViewSet)
-router.register('orders', OrderViewSet)
-router.register('leads', LeadViewSet)
-router.register('wallets', WalletViewSet)
-router.register('commissions', CommissionViewSet)
-router.register('hotdeals', HotDealViewSet)
-router.register('userprofiles', PublicProfileViewSet, basename='profile')
+router.register(r'products', ProductViewSet)
+router.register(r'orders', OrderViewSet)
+router.register(r'leads', LeadViewSet)
+router.register(r'wallets', WalletViewSet)
+router.register(r'commissions', CommissionViewSet)
+router.register(r'hotdeals', HotDealViewSet)
+router.register(r'userprofiles', PublicProfileViewSet)
 
 
 
